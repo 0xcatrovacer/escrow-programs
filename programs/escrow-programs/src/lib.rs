d64a45907e9a1be7309f9eb813ec173fad2a8836
Swarnab Garang <72680953+0xcatrovacer@users.noreply.github.com>
2022-05-27 22:53:55 +0530

    feat: Added impl Initialize block and defined 2 fns to set cpi contexts

--- programs/escrow-programs/src/lib.rs
+++ programs/escrow-programs/src/lib.rs
@@ -1,13 +1,20 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{Mint, TokenAccount};
+use anchor_spl::token::{self, Mint, Transfer, TokenAccount, SetAuthority};
+use spl_token::instruction::AuthorityType;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod escrow_programs {
     use super::*;
+    
+    const ESCROW_PDA_SEED: &[u8] = b"escrow";
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        initializer_amount: u64,
+        taker_amount: u64,
+    ) -> Result<()> {
         Ok(())
     }
 }
@@ -44,13 +51,34 @@ pub struct Initialize<'info> {
     pub system_program: AccountInfo<'info>,
 
     pub rent: Sysvar<'info, Rent>,
-    
+
     pub token_program: AccountInfo<'info>,
+}
+
+impl<'info> Initialize<'info> {
+    fn into_set_authority_context(&self) -> CpiContext<'_, '_, '_, 'info, SetAuthority<'info>> {
+        let cpi_accounts = SetAuthority {
+            account_or_mint: self.vault_account.to_account_info().clone(),
+            current_authority: self.initializer.clone(),
+        };
+
+        CpiContext::new(self.token_program.clone(), cpi_accounts)
+    }
+
+    fn into_transfer_to_pda_context(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
+        let cpi_accounts = Transfer {
+            from: self.initializer_deposit_token_account.to_account_info().clone(),
+            to: self.vault_account.to_account_info().clone(),
+            authority: self.initializer.clone(),
+        };
+
+        CpiContext::new(self.token_program.clone(), cpi_accounts)
+    }
 }
 
 #[account]
 pub struct EscrowAccount {
-    pub initializere_key: Pubkey,
+    pub initializer_key: Pubkey,
     pub initializer_deposit_token_account: Pubkey,
     pub initializer_receive_token_account: Pubkey,
     pub initializer_amount: u64,
