63510f10b340c267d5086dac1ca6edb91b019319
Swarnab Garang <72680953+0xcatrovacer@users.noreply.github.com>
2022-05-27 21:35:39 +0530

    feat: Added Initialize account struct

--- programs/escrow-programs/src/lib.rs
+++ programs/escrow-programs/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint, TokenAccount};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -6,13 +7,46 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod escrow_programs {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(initializer_amount: u64)]
+pub struct Initialize<'info> {
+    #[account(zero)]
+    pub escrow_account: Account<'info, EscrowAccount>,
+
+    #[account(
+        init,
+        seeds = [b"token-seeds".as_ref()],
+        bump,
+        payer = initializer,
+        token::mint = mint,
+        token::authority = initializer
+    )]
+    pub vault_account: Account<'info, TokenAccount>,
+
+    pub mint: Account<'info, Mint>,
+    
+    #[account(mut, signer)]
+    pub initializer: AccountInfo<'info>,
+
+    #[account(
+        mut,
+        constraint = initializer_deposit_token_account.amount >= initializer_amount
+    )]
+    pub initializer_deposit_token_account: Account<'info, TokenAccount>,
+
+    pub initializer_receive_token_account: Account<'info, TokenAccount>,
+
+    pub system_program: AccountInfo<'info>,
+
+    pub rent: Sysvar<'info, Rent>,
+    
+    pub token_program: AccountInfo<'info>,
+}
 
 #[account]
 pub struct EscrowAccount {
